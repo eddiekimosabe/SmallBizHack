409255366711b3634b9c07979610a5d6e3c2fad1
Alexei Pesic <aly.pesic@gmail.com>
2014-09-13 13:27:56 -0400

    Added login/logout views, routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_one :business
   has_many :invoices, :through => :business
 
-  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   validates :password, :confirmation => true
   validates_length_of :password, :in => 8..20, :on => :create
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   resources :users, :only => [:show, :new, :create]
 
-  get :login => "sessions#new"
-  post :login => "sessions#create"
+  get 'signup' => 'users#new'
 
-  delete :logout => "sessions#destroy"
+  get 'login' => "sessions#new"
+  post 'login' => "sessions#create"
+
+  delete 'logout' => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
