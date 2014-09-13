2645ad73d87c7bdbe367a070c36220a310f357d3
Alexei Pesic <aly.pesic@gmail.com>
2014-09-13 15:33:44 -0400

    Added payer/receiver associations to invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,4 @@
 class Invoice < ActiveRecord::Base
-  belongs_to :business
+  belongs_to :payer, class_name: "Business", foreign_key: :payer_id
+  belongs_to :receiver, class_name: "Business", foreign_key: :receiver_id
 end
