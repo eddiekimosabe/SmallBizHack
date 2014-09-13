8e0993af1fdf1fbb912b6753d6c5bc772d7d895a
Alexei Pesic <aly.pesic@gmail.com>
2014-09-13 13:16:13 -0400

    User and sessions controller + routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_one :business
+  has_many :invoices, :through => :business
+
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+  validates :password, :confirmation => true
+  validates_length_of :password, :in => 8..20, :on => :create
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   get 'invoices/new'
 
   get 'businesses/new'
+  resources :users, :only => [:show, :new, :create]
+
+  get :login => "sessions#new"
+  post :login => "sessions#create"
+
+  delete :logout => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
