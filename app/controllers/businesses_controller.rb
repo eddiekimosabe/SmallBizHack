a20c6d49307fa36750284e7a8d5d5d6db739754a
eddiekimosabe <eddiekimosabe@gmail.com>
2014-09-13 16:36:33 -0400

    Setup CRUD for businesses
    -Edit view/method for Business
    -Update method for Business
    -Delete method for Business

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -7,20 +7,34 @@ class BusinessesController < ApplicationController
     @businesses = Business.all
   end
 
-  def edit
-  end
-
   def create
     @business = Business.new(business_params)
     if @business.save
       redirect_to @business
     else
-      redirect_to new_business
+      redirect_to new_business_path
     end
   end
 
   def show
     @business = Business.find(params[:id])
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    @business.update(business_params)
+    redirect_to @business
+  end
+
+  def edit
+    @business = Business.find(params[:id])
+
+  end
+
+  def destroy
+      @business = Business.find(params[:id])
+      @business.destroy
+      redirect_to businesses_path
   end
 
   def business_params
