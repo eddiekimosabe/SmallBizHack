f8523bfd66c46c98bb385d6101707e96718ed37e
Alexei Pesic <aly.pesic@gmail.com>
2014-09-13 13:19:57 -0400

    Views for signup/login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  # signup GET    /signup(.:format)         users#new
+  #        POST   /signup(.:format)         users#create
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect_to posts_path
+    else
+      flash.now.alert = "Invalid email or password"
+      render :new
+    end
+  end
+
+  protected
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
