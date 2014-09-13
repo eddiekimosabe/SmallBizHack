e7a1e97057f29d369f61e2483a4d9188a0878f84
Alexei Pesic <aly.pesic@gmail.com>
2014-09-13 14:26:49 -0400

    Basic index view, nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :users, :only => [:show, :new, :create]
 
   get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
 
   get 'login' => "sessions#new"
   post 'login' => "sessions#create"
