05fe679a03efa47964c9072416c874d092901565
Alexei Pesic <aly.pesic@gmail.com>
2014-09-13 16:28:12 -0400

    Added validations to business and invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,15 +2,15 @@ class Invoice < ActiveRecord::Base
   belongs_to :payer, class_name: "Business", foreign_key: :payer_id
   belongs_to :receiver, class_name: "Business", foreign_key: :receiver_id
 
-  def type(business)
-  	
-  	if business.id == receiver_id
-  		return "receivable"
-  	elsif business.id == payer_id
-  		return  "payable"
-  	else 
-  		return nil
-  	end	
+  validates_presence_of :amount, :payer_id, :receiver_id, :due_date, :received_date
 
+  def type(business)
+    if business.id == receiver_id
+      return "receivable"
+    elsif business.id == payer_id
+      return  "payable"
+    else
+      return nil
+    end
   end
 end
