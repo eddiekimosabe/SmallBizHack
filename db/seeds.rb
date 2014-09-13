6404b8c457c758bee79d79131b3f3817c9a243c4
Alexei Pesic <aly.pesic@gmail.com>
2014-09-13 14:43:58 -0400

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+20.times do
+  Business.create(name: Faker::Company.name)
+end
+
+User.all.each do |user|
+  Business.create(name: Faker::Company.name, user_id: user.id)
+  user.business.payable_invoices.create(
+    amount: rand(100..1000),
+    due_date: rand(60).days.from_now,
+    received_date: Date.now,
+    receiver_id: Business.all.sample
+  )
+  user.business.receivable_invoices.create(
+    amount: rand(100..1000),
+    due_date: rand(60).days.from_now,
+    received_date: Date.now,
+    payable_id: Business.all.sample
+  )
+end
