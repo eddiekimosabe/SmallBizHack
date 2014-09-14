941defe04c047c48fa264d72528bc71c60055ce3
hab278 <hab278@nyu.edu>
2014-09-14 00:56:42 -0400

    prepare welcome page for chart

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
+  include ChartsHelper
   before_action :current_user
-
 
   def index
     @business = @user.business if current_user
+    set_up_vars_for_charts_api
   end
 end
