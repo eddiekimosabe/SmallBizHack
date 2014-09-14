9fddb9e42bb69810a1994d5285dcf70b511100ce
hab278 <hab278@nyu.edu>
2014-09-14 01:03:33 -0400

    Normalize spaces

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   include ChartsHelper
   before_action :current_user
 
+
   def index
     @business = @user.business if current_user
     set_up_vars_for_charts_api
